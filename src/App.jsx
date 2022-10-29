24c1f943a30513862b48df1c00370b21a7456311
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-29 14:24:31 +0300

    Clear Project and Add Login and Home Page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { Login } from './components/Login'
+import { Home } from './container/Home'
+
+export const App = () => {
+	return (
+		<Routes>
+			<Route path='login' element={<Login />} />
+			<Route path='*' element={<Home />} />
+		</Routes>
+	)
+}
